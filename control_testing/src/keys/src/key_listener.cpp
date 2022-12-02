0ddbcd5bdadb3d8cb7c1f81c4c1feb0ea1f2debf
Derek Ward <derekbenjward@gmail.com>
2022-12-02 16:22:01 -0700

    updated cmake file to include curses. used curses library to read keypresses (not buffered) and send them across a topic. Modified the message of the topic from int8 to int16 to incorporate all chars that ncurses can read.

--- control_testing/src/keys/src/key_listener.cpp
+++ control_testing/src/keys/src/key_listener.cpp
@@ -1,29 +1,91 @@
 #include "ros/ros.h"
 #include "keys/Key.h"
+#include "curses.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+
+    // CURSES
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    WINDOW *win = newwin(LINES - 1, COLS - 1, 0, 0);
+    nodelay(win, TRUE);
+    scrollok(win, TRUE);
+    wmove(win, 0, 0);
+
+    // ros initialization code
     ros::init(argc, argv, "key_listener_sender");
     ros::NodeHandle n;
 
+    // ros publisher
     ros::Publisher keys_pub = n.advertise<keys::Key>("keys_heard", 1000);
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(100);
+
+    // start loop for listening for keypresses!
 
     keys::Key new_key;
-    while(ros::ok())
+    int count = 0;
+    while (ros::ok())
     {
-        new_key.keypress = getchar();
-        if (new_key.keypress >= 0)
+        if ((new_key.keypress = getch()) == ERR)
         {
-            ROS_INFO("Saw Keypress: [%c]", new_key.keypress);
-            keys_pub.publish(new_key);
+            //continue; we don't just want to continue... we want ros to take over and limit our message passing frequency... just have it skip the conditionals
+            // by not doing anything.
+        }
+        else if (new_key.keypress == 'x')
+        {
+            ros::shutdown();
         }
         else
         {
-            ROS_INFO("No keypress detected...");
+            count++;
+            switch (new_key.keypress)
+            {
+            case KEY_UP:
+                wprintw(win, "%d: [up]\n", count);
+                wrefresh(win);
+                //ROS_INFO("Saw Keypress: [%d]\n", 1);
+                new_key.keypress = 1;
+                keys_pub.publish(new_key);
+                break;
+            case KEY_DOWN:
+                wprintw(win, "%d: [down]\n", count);
+                wrefresh(win);
+                //ROS_INFO("Saw Keypress: [%d]\n", 2);
+                new_key.keypress = 2;
+                keys_pub.publish(new_key);
+                break;
+            case KEY_LEFT:
+                wprintw(win, "%d: [left]\n", count);
+                wrefresh(win);
+                //ROS_INFO("Saw Keypress: [%d]\n", 3);
+                new_key.keypress = 3;
+                keys_pub.publish(new_key);
+                break;
+            case KEY_RIGHT:
+                wprintw(win, "%d: [right]\n", count);
+                wrefresh(win);
+                //ROS_INFO("Saw Keypress: [%d]\n", 4);
+                new_key.keypress = 4;
+                keys_pub.publish(new_key);
+                break;
+            case KEY_RESIZE:
+                wprintw(win, "%d: [RESIZE]\n", count);
+                wresize(win, LINES - 1, COLS - 1);
+                break;
+            default:
+                wprintw(win, "%d: ~~~\n", count);
+                wrefresh(win);
+                //ROS_INFO("Saw Keypress: [~~~]");
+                //keys_pub.publish(new_key);
+                break;
+            }
         }
-
-        ros::spinOnce(); // not necesary here: added to help remind that if we ever needed a subscription this calls the callback
+        ros::spinOnce();   // not necesary here: added to help remind that if we ever needed a subscription this calls the callback
         loop_rate.sleep(); // sleep the remaining time necesary until we meet the desired frequency
     }
+
+    endwin();
 }
--- control_testing/src/keys/src/key_receiver.cpp
+++ control_testing/src/keys/src/key_receiver.cpp
@@ -1,9 +1,9 @@
 #include "ros/ros.h"
 #include "keys/Key.h"
 
-void keyCallback(const keys::Key::ConstPtr& key);
+void keyCallback(const keys::Key::ConstPtr &key);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "key_receiver");
     ros::NodeHandle n;
@@ -14,5 +14,22 @@ int main(int argc, char** argv)
 
 void keyCallback(const keys::Key::ConstPtr &key)
 {
-    ROS_INFO("I heard key [%c]", (char)key->keypress);
+    switch (key->keypress)
+    {
+    case 1:
+        ROS_INFO("I heard key [up]");
+        break;
+    case 2:
+        ROS_INFO("I heard key [down]");
+        break;
+    case 3:
+        ROS_INFO("I heard key [left]");
+        break;
+    case 4:
+        ROS_INFO("I heard key [right]");
+        break;
+    default:
+        ROS_INFO("something's wrong...");
+        break;
+    }
 }
